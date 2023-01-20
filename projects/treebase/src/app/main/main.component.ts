ae13f6c24a8ee40ea4f1a5706f2469d25b6c74bb
Adam Kariv <adam.kariv@gmail.com>
2023-01-20 23:10:23 +0200

    Initial commit

---
+++ projects/treebase/src/app/main/main.component.ts
@@ -0,0 +1,14 @@
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { delay } from 'rxjs';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.less']
+})
+export class MainComponent {
+
+  constructor() {}
+
+}
